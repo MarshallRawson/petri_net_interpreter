504d3e3eceeb0a396da36d55e492e35efd154046
MarshallRawson <marshallrawson@ufl.edu>
2020-07-08 14:07:31 -0400

    autopep8 formatting

--- petri_net_interpreter/os_features.py
+++ petri_net_interpreter/os_features.py
@@ -2,194 +2,196 @@
 from abc import ABC, abstractmethod
 from petri_net_interpreter.graph_entities import Place, Transition
 
+
 class OsFeature(ABC):
-  def __init__(self, parent):
-    self.parent = parent
+    def __init__(self, parent):
+        self.parent = parent
 
-  @abstractmethod
-  def prototype(self):
-    pass
+    @abstractmethod
+    def prototype(self):
+        pass
 
-  def define(self):
-    return ''
+    def define(self):
+        return ''
 
-  @abstractmethod
-  def initialize(self):
-    pass
+    @abstractmethod
+    def initialize(self):
+        pass
 
 
 class Debug(OsFeature):
-  def __init__(self, parent):
-    super().__init__(parent)
+    def __init__(self, parent):
+        super().__init__(parent)
 
-  def string_len(self):
-    all_names = ''
-    for name in self.parent.places.keys():
-      all_names += name
-    return len(all_names) + len(self.parent.places.keys())*6
+    def string_len(self):
+        all_names = ''
+        for name in self.parent.places.keys():
+            all_names += name
+        return len(all_names) + len(self.parent.places.keys()) * 6
 
-  def get_debug_state(self):
-    s = '// Get Debug state\n'
-    s += 'char debug_state[' + str(self.string_len()) + '];\n'
-    s += 'snprintf(debug_state, ' + str(self.string_len())+ ', "'
-    for name in self.parent.places.keys():
-      s += name + ': %d\\n'
-    s += '\\n", \n'
-    for place in self.parent.places.values():
-      s += place.output.get_status() + ',\n'
-    i = s.rfind(',')
-    s = s[:i] + s[i+1:]
-    s += ');\n'
-    return s
+    def get_debug_state(self):
+        s = '// Get Debug state\n'
+        s += 'char debug_state[' + str(self.string_len()) + '];\n'
+        s += 'snprintf(debug_state, ' + str(self.string_len()) + ', "'
+        for name in self.parent.places.keys():
+            s += name + ': %d\\n'
+        s += '\\n", \n'
+        for place in self.parent.places.values():
+            s += place.output.get_status() + ',\n'
+        i = s.rfind(',')
+        s = s[:i] + s[i + 1:]
+        s += ');\n'
+        return s
 
-  def prototype(self):
-    return ''
+    def prototype(self):
+        return ''
 
-  def initialize(self):
-    return ''
+    def initialize(self):
+        return ''
 
-  def call(self, give_string):
-    return give_string
+    def call(self, give_string):
+        return give_string
 
 
 class ProccessOutput(OsFeature):
-  def __init__(self, parent):
-    super().__init__(parent)
+    def __init__(self, parent):
+        super().__init__(parent)
 
-  @abstractmethod
-  def is_ready(self):
-    pass
+    @abstractmethod
+    def is_ready(self):
+        pass
 
-  @abstractmethod
-  def take(self, val):
-    pass
+    @abstractmethod
+    def take(self, val):
+        pass
 
-  @abstractmethod
-  def give(self, val):
-    pass
+    @abstractmethod
+    def give(self, val):
+        pass
 
-  @abstractmethod
-  def close(self):
-    pass
+    @abstractmethod
+    def close(self):
+        pass
+
 
 class InterProccessCommunication(ProccessOutput):
-  def __init__(self, place):
-    super().__init__(place)
-    self.name = str(place.node) + '_OUT_IPC'
+    def __init__(self, place):
+        super().__init__(place)
+        self.name = str(place.node) + '_OUT_IPC'
 
-  def give(self, val):
-    return self.enqueue(val)
+    def give(self, val):
+        return self.enqueue(val)
 
-  def take(self, val):
-    return self.dequeue(val)
+    def take(self, val):
+        return self.dequeue(val)
 
-  @abstractmethod
-  def enqueue(self, val):
-    pass
+    @abstractmethod
+    def enqueue(self, val):
+        pass
 
-  @abstractmethod
-  def dequeue(self, val):
-    pass
+    @abstractmethod
+    def dequeue(self, val):
+        pass
 
-  def is_ready(self):
-    return self.check_for_new_data()
+    def is_ready(self):
+        return self.check_for_new_data()
 
-  @abstractmethod
-  def check_for_new_data(self):
-    pass
+    @abstractmethod
+    def check_for_new_data(self):
+        pass
 
-  @abstractmethod
-  def get_size(self):
-    pass
+    @abstractmethod
+    def get_size(self):
+        pass
 
-  def get_status(self):
-    return self.get_size()
+    def get_status(self):
+        return self.get_size()
 
-  def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
 
 
 class Semaphore(ProccessOutput):
-  def __init__(self, place, suffix='_OUT_SEMAPHORE'):
-    super().__init__(place)
-    self.name = str(place.node) + suffix
+    def __init__(self, place, suffix='_OUT_SEMAPHORE'):
+        super().__init__(place)
+        self.name = str(place.node) + suffix
 
-  def give(self, val):
-    return self.signal()
+    def give(self, val):
+        return self.signal()
 
-  @abstractmethod
-  def signal(self):
-    pass
+    @abstractmethod
+    def signal(self):
+        pass
 
-  def take(self, val):
-    return self.wait()
+    def take(self, val):
+        return self.wait()
 
-  @abstractmethod
-  def wait(self):
-    pass
+    @abstractmethod
+    def wait(self):
+        pass
 
-  @abstractmethod
-  def get_value(self):
-    pass
+    @abstractmethod
+    def get_value(self):
+        pass
 
-  def get_status(self):
-    return self.get_value()
+    def get_status(self):
+        return self.get_value()
 
-  def is_ready(self):
-    return '(' + self.get_value() + ' > 0)'
+    def is_ready(self):
+        return '(' + self.get_value() + ' > 0)'
 
-  def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
 
 
 class OperatingSystem(ABC):
-  def __init__(self, ipc, sem, place, transition, debug=Debug):
-    self.ipc = ipc
-    if not issubclass(ipc, InterProccessCommunication):
-      raise Exception('ipc must inherit from InterProccessCommunication')
-    self.sem = sem
-    if not issubclass(sem, Semaphore):
-      raise Exception('sem must inherit from Semaphore')
-    self.place = place
-    if not issubclass(place, Place):
-      raise Exception('place must inherit from Place')
-    self.transition = transition
-    if not issubclass(transition, Transition):
-      raise Exception('transition must inherit from Transition')
-    self.debug = debug
-    if not issubclass(debug, Debug):
-      raise Exception('debug must inherit from Debug')
+    def __init__(self, ipc, sem, place, transition, debug=Debug):
+        self.ipc = ipc
+        if not issubclass(ipc, InterProccessCommunication):
+            raise Exception('ipc must inherit from InterProccessCommunication')
+        self.sem = sem
+        if not issubclass(sem, Semaphore):
+            raise Exception('sem must inherit from Semaphore')
+        self.place = place
+        if not issubclass(place, Place):
+            raise Exception('place must inherit from Place')
+        self.transition = transition
+        if not issubclass(transition, Transition):
+            raise Exception('transition must inherit from Transition')
+        self.debug = debug
+        if not issubclass(debug, Debug):
+            raise Exception('debug must inherit from Debug')
 
-  @staticmethod
-  def place_body_name(place):
-    return str(place.node) + '_body'
+    @staticmethod
+    def place_body_name(place):
+        return str(place.node) + '_body'
 
-  @staticmethod
-  def place_wrapper_name(place):
-    return str(place.node) + '_wrapper'
+    @staticmethod
+    def place_wrapper_name(place):
+        return str(place.node) + '_wrapper'
 
-  @staticmethod
-  def header_start():
-    return "#pragma once"
+    @staticmethod
+    def header_start():
+        return "#pragma once"
 
-  @staticmethod
-  def header_file_end():
-    return ''
+    @staticmethod
+    def header_file_end():
+        return ''
 
-  @staticmethod
-  @abstractmethod
-  def includes():
-    pass
+    @staticmethod
+    @abstractmethod
+    def includes():
+        pass
 
-  @staticmethod
-  @abstractmethod
-  def kill_self():
-    pass
+    @staticmethod
+    @abstractmethod
+    def kill_self():
+        pass
 
-  @classmethod
-  @abstractmethod
-  def add_thread(cls, place, param):
-    pass
+    @classmethod
+    @abstractmethod
+    def add_thread(cls, place, param):
+        pass
 
-  def initialize(self):
-    return ''
+    def initialize(self):
+        return ''
--- petri_net_interpreter/state_graph.py
+++ petri_net_interpreter/state_graph.py
@@ -2,57 +2,60 @@
 import warnings
 import pygraphviz as pgv
 
+
 class StateGraph(object):
-  def __init__(self, petri_net):
-    self.state_graph = {}
-    self.petri_net = petri_net
-    self.graph = None
+    def __init__(self, petri_net):
+        self.state_graph = {}
+        self.petri_net = petri_net
+        self.graph = None
 
-  def generate(self):
-    transition0 = self.petri_net.transitions[self.petri_net.transition0()]
-    start_state = {}
-    for name in self.petri_net.places.keys():
-      start_state[name] = 0
-    for edge in transition0.outs.keys():
-      start_state[edge] += 1
-    self.explore_state(start_state)
-    self.graph = pgv.AGraph(self.state_graph, directed=True)
+    def generate(self):
+        transition0 = self.petri_net.transitions[self.petri_net.transition0()]
+        start_state = {}
+        for name in self.petri_net.places.keys():
+            start_state[name] = 0
+        for edge in transition0.outs.keys():
+            start_state[edge] += 1
+        self.explore_state(start_state)
+        self.graph = pgv.AGraph(self.state_graph, directed=True)
 
-  def explore_state(self, prev_state):
-    if str(prev_state) not in self.state_graph.keys():
-      self.state_graph[str(prev_state)] = []
-    next_states = self.next_states(prev_state)
-    for state in next_states:
-      self.state_graph[str(prev_state)].append(str(state))
-      if str(state) not in self.state_graph.keys():
-        if sum(state.values()) > self.petri_net.max_tokens():
-          warnings.warn('State ' + str(state) + ' has more than the max number of tokens, this state will not be explored', RuntimeWarning)
-        else:
-          self.explore_state(state)
+    def explore_state(self, prev_state):
+        if str(prev_state) not in self.state_graph.keys():
+            self.state_graph[str(prev_state)] = []
+        next_states = self.next_states(prev_state)
+        for state in next_states:
+            self.state_graph[str(prev_state)].append(str(state))
+            if str(state) not in self.state_graph.keys():
+                if sum(state.values()) > self.petri_net.max_tokens():
+                    warnings.warn(
+                        'State ' +
+                        str(state) +
+                        ' has more than the max number of tokens, this state will not be explored',
+                        RuntimeWarning)
+                else:
+                    self.explore_state(state)
 
-  def next_states(self, state):
-    # find all the transitions that could fire at this state
-    next_states = []
-    for transition in self.petri_net.transitions.values():
-      in_places = [edge for edge in transition.ins.keys()]
-      out_places = [edge for edge in transition.outs.keys()]
-      availble_tokens = [state[place] for place in in_places]
-      if 0 not in availble_tokens and len(availble_tokens) > 0:
-        # take a token from all input places
-        new_state = state.copy()
-        for place in in_places:
-          new_state[place] -= 1
-        # add a token to all output places
-        for place in out_places:
-          new_state[place] += 1
-        next_states.append(new_state)
-    return next_states
+    def next_states(self, state):
+        # find all the transitions that could fire at this state
+        next_states = []
+        for transition in self.petri_net.transitions.values():
+            in_places = [edge for edge in transition.ins.keys()]
+            out_places = [edge for edge in transition.outs.keys()]
+            availble_tokens = [state[place] for place in in_places]
+            if 0 not in availble_tokens and len(availble_tokens) > 0:
+                # take a token from all input places
+                new_state = state.copy()
+                for place in in_places:
+                    new_state[place] -= 1
+                # add a token to all output places
+                for place in out_places:
+                    new_state[place] += 1
+                next_states.append(new_state)
+        return next_states
 
-  def to_dot(self, dot_file):
-    self.graph.write(dot_file)
+    def to_dot(self, dot_file):
+        self.graph.write(dot_file)
 
-  def to_png(self, png_file):
-    self.graph.layout()
-    self.graph.draw(png_file)
-
-
+    def to_png(self, png_file):
+        self.graph.layout()
+        self.graph.draw(png_file)
